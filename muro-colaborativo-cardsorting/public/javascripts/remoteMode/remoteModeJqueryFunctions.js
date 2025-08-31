16ff967eea80dd5fbdcdefa112aefa9cfb643814
joshuaSMP <joshuasmp_2001@ciencias.unam.mx>
2025-08-31 14:23:25 -0600

    Correcion sobreposicion de texto,imagen y link

--- muro-colaborativo-cardsorting/public/javascripts/remoteMode/remoteModeJqueryFunctions.js
+++ muro-colaborativo-cardsorting/public/javascripts/remoteMode/remoteModeJqueryFunctions.js
@@ -31,6 +31,11 @@ let contenido = "";
 
 //  START ready function
 $(document).ready(function () {
+  // Move modals to be direct children of `body` to escape any
+  // stacking contexts that could limit their z-index. This is a robust
+  // way to ensure they always appear on top of other page content.
+  $("#selectMosaicSpace, #workspaceMode, #SubirFoto, #linkMode").appendTo("body");
+
   ValidarStatus();
   $('.toggle-icon').click();
   getContributionslist();
